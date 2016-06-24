b35a08b206417452d70a5807a593bdab33be985b
Enrique Jimenez Campos <serialsito@gmail.com>
2016-06-24 14:18:46 +0200

    Update license

--- recipes/prepare_system.rb
+++ recipes/prepare_system.rb
@@ -1,4 +1,11 @@
-
+#
+# Cookbook Name:: manager
+# Recipe:: prepare_system
+#
+# Copyright 2016, redborder
+#
+# AFFERO GENERAL PUBLIC LICENSE V3
+#
 managers = get_managers()
 
 node.set["redborder"]["memory"] = memory(12345678)
