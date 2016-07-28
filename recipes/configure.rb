059ca4d664670b3998a96bc20c7d1a88b7e5d6d3
Enrique Jimenez <serialsito@gmail.com>
2016-07-28 15:01:36 +0200

    uncomment kafka service

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -14,14 +14,14 @@ zookeeper_config "Configure Zookeeper" do
   action (node["redborder"]["services"]["zookeeper"] ? :add : :remove)
 end
 
-#kafka_config "Configure Kafka" do
-#  memory node["redborder"]["memory_services"]["kafka"]["memory"]
-#  maxsize node["redborder"]["manager"]["hd_services_current"]["kafka"]
-#  managers_list node["redborder"]["managers_per_services"]["kafka"]
-#  zk_hosts node["redborder"]["zookeeper"]["zk_hosts"]
-#  host_index node["redborder"]["kafka"]["host_index"]
-#  action (node["redborder"]["services"]["kafka"] ? :add : :remove)
-#end
+kafka_config "Configure Kafka" do
+  memory node["redborder"]["memory_services"]["kafka"]["memory"]
+  maxsize node["redborder"]["manager"]["hd_services_current"]["kafka"]
+  managers_list node["redborder"]["managers_per_services"]["kafka"]
+  zk_hosts node["redborder"]["zookeeper"]["zk_hosts"]
+  host_index node["redborder"]["kafka"]["host_index"]
+  action (node["redborder"]["services"]["kafka"] ? :add : :remove)
+end
 
 druid_coordinator "Configure Druid Coordinator" do
   name node["hostname"]
