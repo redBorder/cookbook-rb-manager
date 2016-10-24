86de68b82363e5b6b854f7cc756bfcc8ea6744b0
cjmateos <mateos83@gmail.com>
2016-10-24 11:57:30 +0200

    Fix in zookeeper host attribute

--- resources/recipes/prepare_system.rb
+++ resources/recipes/prepare_system.rb
@@ -64,7 +64,7 @@ node.default["redborder"]["sensors_info"] = get_sensors_info()
 
 #get string with all zookeeper hosts and port separated by commas, its needed for multiples services
 zk_port = node["redborder"]["zookeeper"]["port"]
-zk_hosts = node["redborder"]["managers_per_services"]["zookeeper"].map {|z| "#{z}:#{zk_port}"}.join(',')
+zk_hosts = node["redborder"]["managers_per_services"]["zookeeper"].map {|z| "#{z}.node:#{zk_port}"}.join(',')
 node.default["redborder"]["zookeeper"]["zk_hosts"] = zk_hosts
 
 #set kafka host index if kafka is enabled in this host
