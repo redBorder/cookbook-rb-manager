e2d3a717222159bb4f2ce0fa97a21383c168dc66
Jordi_Hernandez <jhernandez@redborder.com>
2021-11-19 08:54:57 +0000

    fill get_orgs helper

--- resources/libraries/get_orgs.rb
+++ resources/libraries/get_orgs.rb
@@ -1,15 +1,19 @@
 module Rb_manager
   module Helpers
     def get_orgs()
-return []
-    #  Chef::Role.list.keys.each do |m_key|
-    #    m = Chef::Role.load m_key
-    #    if !m.override_attributes["redBorder"].nil? and !m.override_attributes["redBorder"]["organization_uuid"].nil? and m.override_attributes["redBorder"]["sensor_uuid"] == m.override_attributes["redBorder"]["organization_uuid"]
-    #      organizations << m
-    #    end
-    #  end
-    #  return organizations
+
+      organizations = []
+
+      Chef::Role.list.keys.each do |m_key|
+        m = Chef::Role.load m_key
+        if !m.override_attributes["redborder"].nil? and !m.override_attributes["redborder"]["organization_uuid"].nil? and m.override_attributes["redborder"]["sensor_uuid"] == m.override_attributes["redborder"]["organization_uuid"]
+          organizations << m
+        end
+      end
+
+      organizations
 
     end
   end
 end
+
