3fa810a11ce9a156b6b7c709b0ff7ff565811468
davidredborder <34706472+davidredborder@users.noreply.github.com>
2022-01-21 12:03:43 +0000

    fix enable/disable services (#26)

--- resources/libraries/manager_services.rb
+++ resources/libraries/manager_services.rb
@@ -1,7 +1,13 @@
 class Chef
   class Recipe
-    def manager_services(manager)
-      # ... your code here ... 
+    def manager_services()
+      manager_services  = {}
+      node["redborder"]["services"].each { |k,v| manager_services[k] = v if (v==true or v==false) } if !node["redborder"]["services"].nil?
+      
+      # changing default values in case of the user has modify them
+      node["redborder"]["services"]["overwrite"].each { |k,v| manager_services[k] = v if (v==true or v==false) } if !node["redborder"]["services"]["overwrite"].nil?
+      
+      return manager_services
     end
   end
 end
