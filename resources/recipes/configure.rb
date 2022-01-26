30cfbe4f2e7980e6fc15063353c3f3ba8034f9ae
eareyes <eareyes@redborder.com>
2022-01-26 08:29:18 +0000

    Adapt social and n2klocd services

--- resources/recipes/configure.rb
+++ resources/recipes/configure.rb
@@ -235,7 +235,7 @@ end
 rbsocial_config "Configure redborder-social" do
   social_nodes node["redborder"]["sensors_info_all"]["social-sensor"]
   memory node["redborder"]["memory_services"]["redborder-social"]["memory"]
-  action (node["redborder"]["services"]["redborder-social"] ? [:add, :register] : [:remove, :deregister])
+  action (manager_services["redborder-social"] ? [:add, :register] : [:remove, :deregister])
 end
 
 rsyslog_config "Configure rsyslog" do
@@ -256,7 +256,7 @@ n2klocd_config "Configure n2klocd" do
   meraki_nodes node["redborder"]["sensors_info_all"]["meraki-sensor"]
   n2klocd_managers node["redborder"]["managers_per_services"]["n2klocd"]
   memory node["redborder"]["memory_services"]["n2klocd"]["memory"]
-  action (node["redborder"]["services"]["n2klocd"] ? [:add, :register] : [:remove, :deregister])
+  action (manager_services["n2klocd"] ? [:add, :register] : [:remove, :deregister])
 end
 
 # Determine external
