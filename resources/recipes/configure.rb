0252503470cc070caafd29440634d6bdb706becb
vimesa <vimesa@redborder.com>
2022-03-28 08:56:49 +0100

    Added device sensor

--- resources/recipes/configure.rb
+++ resources/recipes/configure.rb
@@ -171,6 +171,10 @@ end
 
 rbscanner_config "Configure redborder-scanner" do
   scanner_nodes node["redborder"]["sensors_info_all"]["scanner-sensor"]
+  device_nodes node["redborder"]["sensors_info_all"]["device-sensor"]
+  flow_nodes node["redborder"]["sensors_info_all"]["flow-sensor"]
+  kafka_managers node["redborder"]["managers_per_services"]["kafka"]
+  managers node["redborder"]["managers_list"]
   action (manager_services["redborder-scanner"] ? [:add, :register] : [:remove, :deregister])
 end
 
