92920fe9d1c1a609bf6bb337b4e89a9987cf9d7e
Javier Rodriguez Gomez <javiercrg@redborder.com>
2022-04-22 10:25:46 +0100

    Moved to cron_d syntax, removed cron job templates

--- resources/recipes/configure_cron_tasks.rb
+++ resources/recipes/configure_cron_tasks.rb
@@ -2,124 +2,131 @@ manager_services = manager_services()
 cluster_installed = File.exist?("/etc/cluster-installed.txt")
 
 #--------------------------Druid-------------------------#
-if (manager_services["druid_coordinator"] or manager_services["druid_historical"]) and cluster_installed
-  template "/etc/cron.daily/rb_clean_segments.sh" do
-    source "rb_clean_segments_cron.erb"
-    owner "root"
-    group "root"
-    mode 0755
+if (manager_services["druid-coordinator"] or manager_services["druid-historical"]) and cluster_installed
+  cron_d 'clean_segments_daily' do
+    action :create
+    minute '00'
+    hour   '01'
+    weekday '*'
     retries 2
     ignore_failure true
+    command "/usr/lib/redborder/bin/rb_clean_segments.sh"
   end
-elsif File.exists?("/etc/cron.daily/rb_clean_segments.sh")
-  file "/etc/cron.daily/rb_clean_segments.sh" do
+else
+  cron_d 'clean_segments_daily' do
     action :delete
   end
 end
 
-template "/etc/cron.daily/create_druid_metadata.sh" do
-  source "create_druid_metadata_cron.erb"
-  owner "root"
-  group "root"
-  mode 0755
+cron_d 'create_druid_metadata_daily' do
+  action :create
+  minute '00'
+  hour   '00'
+  weekday '*'
   retries 2
+  ignore_failure true
+  command "/usr/lib/redborder/bin/rb_create_druid_metadata.sh"
 end
 
 #--------------------------REPOS-------------------------#
-template "/etc/cron.daily/rb_repo_updates.sh" do
-  source "rb_repo_updates_cron.erb"
-  owner "root"
-  group "root"
-  mode 0755
+cron_d 'repo_updates_daily' do
+  action :create
+  minute '00'
+  hour   '00'
+  weekday '*'
   retries 2
   ignore_failure true
+  command "/usr/lib/redborder/bin/rb_repo_updates.sh"
 end
 
 #--------------------------AWS-------------------------#
+# AWS Cloudwatch needs to be integrated
 if !node["redborder"].nil? and !node["redborder"]["dmidecode"].nil? and !node["redborder"]["dmidecode"]["manufacturer"].nil? and node["redborder"]["iscloud"] and manager_services["awslogs"]
-  template "/etc/cron.d/awsmon" do
-    source "awsmon_cron.erb"
-    owner "root"
-    group "root"
-    mode 0644
+  cron_d 'awsmon_hourly' do
+    action :create
+    minute '5'
+    hour   '*'
+    weekday '*'
     retries 2
+    ignore_failure true
+    environment({'MEM' => node["filesystem"].select {|k,v| k.start_with?"/dev/mapper/"}.map{|k,v| "--disk-path=#{v["mount"]}"}.join(" ") })
+    command '/usr/lib/redborder/bin/rb_awsmon.sh --mem-util $MEM --disk-space-util --from-cron --auto-scaling'
   end
-elsif File.exist?"/etc/cron.d/awsmon"
-  file "/etc/cron.d/awsmon" do
+else
+  cron_d 'awsmon_hourly' do
     action :delete
   end
 end
 
 #--------------------------Rabbitmq-------------------------#
 if manager_services["rabbitmq"] and cluster_installed
-  template "/etc/cron.daily/rabbitmq" do
-    source "rabbitmq_cron.erb"
-    owner "root"
-    group "root"
-    mode 0755
+  cron_d 'rabbitmq_rotate_logs_daily' do
+    action :create
+    minute '00'
+    hour   '01'
+    weekday '*'
     retries 2
+    ignore_failure true
+    command "/usr/lib/redborder/bin/rb_rabbitmq_rotate_logs.sh"
   end
 else
-  if File.exists?("/etc/cron.daily/rabbitmq")
-    file "/etc/cron.daily/rabbitmq" do
-      action :delete
-    end
+  cron_d 'rabbitmq_rotate_logs_daily' do
+    action :delete
   end
 end
 
 #--------------------------Events-counter-------------------------#
-template "/etc/cron.daily/rb_eventscounter.sh" do
-  source "rb_eventscounter_cron.erb"
-  owner "root"
-  group "root"
-  mode 0755
+cron_d 'eventscounter_daily' do
+  action :create
+  minute '00'
+  hour   '00'
+  weekday '*'
   retries 2
   ignore_failure true
+  command 'systemctl restart redborder-events-counter &>/dev/null; exit 0;'
 end
 
 #--------------------------Licenses-------------------------#
-template "/etc/cron.daily/check_licenses_daily.sh" do
-  source "check_licenses_daily_cron.erb"
-  owner "root"
-  group "root"
-  mode 0755
+cron_d 'check_licences_daily' do
+  action :create
+  minute '00'
+  hour   '00'
+  weekday '*'
   retries 2
   ignore_failure true
+  command "/usr/lib/redborder/bin/rb_check_licenses_daily.sh"
 end
 
-template "/etc/cron.weekly/check_licenses_weekly.sh" do
-  source "check_licenses_weekly_cron.erb"
-  owner "root"
-  group "root"
-  mode 0755
+cron_d 'check_licences_weekly' do
+  action :create
+  minute '00'
+  hour   '01'
+  weekday '1'
   retries 2
   ignore_failure true
+  command "/usr/lib/redborder/bin/rb_check_licenses_weekly.sh"
 end
+
 
 #--------------------------Darklist-------------------------#
 # TODO Only the master node should have these cron jobs
 # if (manager_mode == "master")
-template "/etc/cron.weekly/rb_update_darklist.sh" do
-  source "rb_update_darklist_cron.erb"
-  owner "root"
-  group "root"
-  mode 0755
+cron_d 'rb_update_darklist_weekly' do
+  action :create
+  minute '00'
+  hour   '00'
+  weekday '1'
   retries 2
   ignore_failure true
-  notifies :run, 'execute[update_darklist]', :delayed
+  command "/usr/lib/redborder/bin/rb_update_darklist.sh"
 end
 
-execute "update_darklist" do
-  ignore_failure true
-  command "/etc/cron.weekly/rb_update_darklist.sh"
-  action :nothing
-end
-
-template "/etc/cron.hourly/rb_refresh_darklist_memcached_keys.sh" do
-  source "rb_refresh_darklist_memcached_keys_cron.erb"
-  owner "root"
-  group "root"
-  mode 0755
+cron_d 'refresh_darklist_memcached_keys_hourly' do
+  action :create
+  minute '00'
+  hour   '*'
+  weekday '*'
   retries 2
   ignore_failure true
+  command "/usr/lib/redborder/bin/rb_refresh_darklist_memcached_keys.sh"
 end
