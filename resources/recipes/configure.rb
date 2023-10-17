e164027ae33d603dfa3227e6a6f3783c3500dca7
Miguel Negron <manegron@redborder.com>
2023-10-17 21:46:57 +0100

    Disable freeradius for now

--- resources/recipes/configure.rb
+++ resources/recipes/configure.rb
@@ -293,10 +293,10 @@ rbale_config "Configure redborder-ale" do
   action (node["redborder"]["services"]["redborder-ale"] ? [:add, :register] : [:remove, :deregister])
 end
 
-freeradius_config "Configure radiusd" do
-  flow_nodes node["redborder"]["sensors_info_all"]["flow-sensor"]
-  action (node["redborder"]["services"]["radiusd"] ? [:config_common, :config_manager, :register] : [:remove, :deregister])
-end
+#freeradius_config "Configure radiusd" do
+#  flow_nodes node["redborder"]["sensors_info_all"]["flow-sensor"]
+#  action (node["redborder"]["services"]["radiusd"] ? [:config_common, :config_manager, :register] : [:remove, :deregister])
+#end
 
 rbcep_config "Configure redborder-cep" do
   flow_nodes node["redborder"]["sensors_info_all"]["flow-sensor"]
