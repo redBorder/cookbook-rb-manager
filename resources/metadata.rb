f9a20f7d3150d3c872a3d27796805c0fe5b6f367
JuanSheba <jsoto@redborder.com>
2024-06-03 10:00:33 +0100

    Release 1.9.2

--- resources/metadata.rb
+++ resources/metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Eneo Tecnología S.L.'
 maintainer_email 'git@redborder.com'
 license          'AGPL-3.0'
 description      'Installs/Configures redborder manager'
-version          '1.9.0'
+version          '1.9.1'
 
 depends 'rb-common'
 depends 'chef-server'
