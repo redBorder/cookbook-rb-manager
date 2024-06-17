040aedad3ab8b767d21367511188f1981e97467b
nilsver <nverschaeve@redborder.com>
2024-06-17 14:53:07 +0100

    fix linter

--- resources/libraries/get_monitor_in_proxy.rb
+++ resources/libraries/get_monitor_in_proxy.rb
@@ -1,10 +1,8 @@
 module RbManager
   module Helpers
-
     def find_monitor_sensor_in_proxy_nodes
       sensors_info = []
       sensor_types = ['proxy-sensor']
-
       sensor_types.each do |s_type|
         begin
           sensors = search(:node, "role:#{s_type}")
@@ -18,6 +16,5 @@ module RbManager
       end
       sensors_info
     end
-
   end
 end
