c05abf5bca424725193e9857abb7ad5e0777f9a2
nilsver <nverschaeve@redborder.com>
2024-06-20 14:09:09 +0100

    fix linter

--- resources/libraries/get_monitor_configuration.rb
+++ resources/libraries/get_monitor_configuration.rb
@@ -2,7 +2,7 @@ module RbManager
   module Helpers
     def get_monitor_configuration
       monitor_config = []
-      sensor = search(:node, "redborder_monitors:[* TO *] AND name:*device*").sort
+      sensor = search(:node, 'redborder_monitors:[* TO *] AND name:*device*').sort
       sensor.each do |node|
         monitors = node.normal['redborder']['monitors']
         monitors.each do |monitor|
