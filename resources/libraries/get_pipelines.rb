e70f657215577269c40cd2eae51fe048e72b4e82
nilsver <nverschaeve@redborder.com>
2024-06-21 09:35:19 +0100

    change 'or' to 'and' and add same check to mobility pipeline

--- resources/libraries/get_pipelines.rb
+++ resources/libraries/get_pipelines.rb
@@ -17,8 +17,8 @@ module RbManager
         logstash_pipelines.push('sflow-pipeline') unless sensors['flow-sensor'].empty?
         logstash_pipelines.push('meraki-pipeline') unless sensors['meraki-sensor'].empty?
         logstash_pipelines.push('monitor-pipeline') unless namespaces.empty?
-        logstash_pipelines.push('location-pipeline') unless sensors['ale-sensor'].empty? || sensors['mse-sensor'].empty? || sensors['flow-sensor'].empty? || sensors['arubacentral-sensor'].empty?
-        logstash_pipelines.push('mobility-pipeline')
+        logstash_pipelines.push('location-pipeline') unless sensors['ale-sensor'].empty? && sensors['mse-sensor'].empty? && sensors['flow-sensor'].empty? && sensors['arubacentral-sensor'].empty?
+        logstash_pipelines.push('mobility-pipeline') unless sensors['ale-sensor'].empty? && sensors['mse-sensor'].empty? && sensors['flow-sensor'].empty? && sensors['arubacentral-sensor'].empty?
         logstash_pipelines.push('redfish-pipeline') unless sensors['device-sensor'].empty?
         logstash_pipelines.push('bulkstats-pipeline') unless sensors['device-sensor'].empty?
       end
