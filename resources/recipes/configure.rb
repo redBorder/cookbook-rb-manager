3b3eb219222618f0f3c67b97c180d844a336730d
Miguel Negron <manegron@redborder.com>
2024-06-28 10:43:07 +0100

    Configure memcached servers

--- resources/recipes/configure.rb
+++ resources/recipes/configure.rb
@@ -514,7 +514,7 @@ end
 
 mem2incident_config 'Configure redborder-mem2incident' do
   cdomain node['redborder']['cdomain']
-  memcached_servers node['redborder']['managers_per_services']['memcached']
+  memcached_servers node['redborder']['managers_per_services']['memcached'].map { |s| "#{s}:#{node['redborder']['memcached']['port']}" }
   auth_token get_api_auth_token
   if manager_services['redborder-mem2incident']
     action [:add, :register]
