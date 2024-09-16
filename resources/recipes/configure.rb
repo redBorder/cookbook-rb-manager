59f513f8704a8f4e297aba73beacbadcb99a6a8c
Pablo Pérez <pperez@redborder.com>
2024-09-16 12:47:09 +0100

    rename variable

--- resources/recipes/configure.rb
+++ resources/recipes/configure.rb
@@ -547,7 +547,7 @@ end
 
 rb_ai_config 'Configure redborder-ai' do
   ai_selected_model node['redborder']['ai_selected_model']
-  allowed_cpus node['redborder']['ai_allowed_cpus'][node['hostname'].to_sym]
+  ai_allowed_cpus node['redborder']['ai_allowed_cpus'][node['hostname'].to_sym]
   ipaddress node['ipaddress_sync']
   if manager_services['redborder-ai']
     action [:add, :register]
