ae380d260d84ddda3c6cc55cc317644607532a3a
manegron <manegron@redborder.com>
2024-10-11 21:07:13 +0100

    fix bug related with cluster_install variable on prepare_system

--- resources/recipes/prepare_system.rb
+++ resources/recipes/prepare_system.rb
@@ -14,6 +14,8 @@ end
 mode = node['redborder']['mode']
 node['redborder']['services_group'][mode].each { |s| node.default['redborder']['services'][s] = true }
 
+cluster_installed = File.exist?('/etc/redborder/cluster-installed.txt'
+
 if mode != 'core' || mode != 'full'
   node.default['redborder']['services']['consul-client'] = true
 end
