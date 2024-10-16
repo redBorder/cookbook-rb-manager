3a2a4943a336d6f652fa63765c5c52eda85fced6
Miguel Negron <manegron@redborder.com>
2024-10-16 15:10:56 +0100

    Add geoip user

--- resources/recipes/configure.rb
+++ resources/recipes/configure.rb
@@ -286,6 +286,7 @@ samza_config 'Configure samza applications' do
 end
 
 geoip_config 'Configure GeoIP' do
+  user_id node['redborder']['geoip_user']
   license_key node['redborder']['geoip_key']
   action :add
 end
