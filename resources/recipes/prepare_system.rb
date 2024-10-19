81a5c01932bd2546725f6fc387dc84a88afc90c7
manegron <manegron@redborder.com>
2024-10-19 15:34:44 +0100

    Save services enablement into services.json

--- resources/recipes/prepare_system.rb
+++ resources/recipes/prepare_system.rb
@@ -13,8 +13,6 @@ end
 # Set services_group related with the node mode (core, full, ...)
 node['redborder']['services_group'][node['redborder']['mode']].each { |s| node.default['redborder']['services'][s] = true }
 
-node.default['redborder']['services']['consul-client'] = (node['redborder']['mode'] != 'core' && node['redborder']['mode'] != 'full')
-
 node.run_state['cluster_installed'] = File.exist?('/etc/redborder/cluster-installed.txt')
 
 # Set :ipaddress_sync
@@ -200,4 +198,18 @@ hosts_entries.each do |line|
     command "echo '#{line}' >> /etc/hosts"
     not_if "grep -q '^#{line}' /etc/hosts"
   end
+end
+
+# Build service.list for rbcli
+services = node['redborder']['services'] ||  []
+systemd_services = node['redborder']['systemdservices'] || []
+service_enablement = {}
+
+systemd_services.each do |service_name, systemd_name|
+  service_enablement[systemd_name.first] = services[service_name]
+end
+
+Chef::Log.info("Saving services enablement into /etc/redborder/services.json")
+File.open("/etc/redborder/services.json", "w") do |file|
+  file.write(JSON.pretty_generate(service_enablement))
 end
