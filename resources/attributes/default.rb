84f066abc231dce8533aefef10ad1670a030183e
manegron <manegron@redborder.com>
2024-10-23 21:14:24 +0100

    fix attribute

--- resources/attributes/default.rb
+++ resources/attributes/default.rb
@@ -4,7 +4,7 @@ default['redborder']['organization_uuid'] = nil
 default['redborder']['organizations'] = []
 default['redborder']['locations'] = %w(namespace namespace_uuid organization organization_uuid service_provider service_provider_uuid deployment deployment_uuid market market_uuid campus campus_uuid building building_uuid floor floor_uuid)
 default['redborder']['sso_enabled'] = '0'
-default['redborder']['repo'] = []
+default['redborder']['repo'] = {}
 default['redborder']['repo']['version'] = nil
 
 # s3
