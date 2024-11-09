55d679c3406111c167e6dffebc1344fae4d67ae1
Rafa Gómez <rgomez@redborder.com>
2024-11-09 10:45:50 +0000

    First version of harddisk_services NG

--- resources/libraries/harddisk_services.rb
+++ resources/libraries/harddisk_services.rb
@@ -3,48 +3,26 @@ module RbManager
     def harddisk_services
       hd_services = {}
 
-      root_dev = node['redborder']['manager']['data_dev']['root']
-      raw_dev = node['redborder']['manager']['data_dev']['raw']
-      aggregate_dev = node['redborder']['manager']['data_dev']['aggregate']
+      root_dev = node['filesystem']['by_mountpoint']['/']
 
-      root_dev = node['filesystem'].keys.first if node['filesystem'][root_dev].nil?
-      if node['filesystem'][raw_dev].nil? && node['filesystem'][aggregate_dev].nil?
-        raw_dev = root_dev
-        aggregate_dev = root_dev
-      elsif node['filesystem'][raw_dev].nil?
-        raw_dev = aggregate_dev
-      elsif node['filesystem'][aggregate_dev].nil?
-        aggregate_dev = raw_dev
-      end
+      hd_services_dev = { 'root' => root_dev, 'raw' => root_dev, 'aggregate' => root_dev }
 
-      hd_services_dev = {}
-      hd_services_dev['root'] = root_dev
-      hd_services_dev['raw'] = raw_dev
-      hd_services_dev['aggregate'] = aggregate_dev
-
-      hd_services_size = {}
-      hd_services_size[root_dev] = 0
-      hd_services_size[raw_dev] = 0
-      hd_services_size[aggregate_dev] = 0
-
-      hd_services_size_total = {}
-      hd_services_size_total[root_dev] = 0
-      hd_services_size_total[raw_dev] = 0
-      hd_services_size_total[aggregate_dev] = 0
+      hd_services_size = Hash.new(0)
+      hd_services_size_total = Hash.new(0)
 
       maxsize = {}
       hd_services_dev.each do |type, device|
         maxsize[type] = if node['filesystem'][device].nil?
                           300_000_000_000
                         else
-                          filesystem_data = node['filesystem'][device]
-                          root_device = hd_services_dev['root']
-                          size_kb = filesystem_data['kb_size'] || node['filesystem'][root_device]['kb_size']
+                          filesystem_data = root_dev['kb_size']
+                          size_kb = filesystem_data
                           size_bytes = size_kb.to_i * 1024
-                          size_bytes -= 7 * 1024 * 1024 * 1024 if device == root_device
+                          size_bytes -= 7 * 1024 * 1024 * 1024
                           [size_bytes, 0].max
                         end
       end
+
       node['redborder']['manager']['hd_services'].each do |s|
         if node['redborder']['services'][s[:name]]
           hd_services_size[hd_services_dev[s[:prefered]]] += s[:count]
