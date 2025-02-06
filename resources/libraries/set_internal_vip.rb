b87fba80447342b354e8f70ed4fe4ab2694dd1c6
Rafael Gomez <rgomez@redborder.com>
2025-02-06 09:08:21 +0000

    Fix linter

--- resources/libraries/set_internal_vip.rb
+++ resources/libraries/set_internal_vip.rb
@@ -1,6 +1,5 @@
 module RbManager
   module Helpers
-
     def find_vip_from_serf(serf_key)
       serf_output = `serf members`
       master_ip = serf_output.lines.find do |line|
@@ -8,10 +7,10 @@ module RbManager
       end
       master_ip ? master_ip.split[1].split(':')[0] : nil
     end
-    
+
     def update_hosts_file(hosts_file, master_vip, service, vip)
       return false if ::File.readlines(hosts_file).any? { |line| line =~ /#{master_vip}\s+#{service.gsub('.', '\.')}/ }
- 
+
       hosts_content = ::File.read(hosts_file).lines.reject { |line| line.include?(service) }
       hosts_content << "#{master_vip} #{service}\n"
       ::File.open(hosts_file, 'w') { |file| file.puts hosts_content }
@@ -26,14 +25,14 @@ module RbManager
         system("/usr/lib/redborder/bin/rb_sync_from_master.sh #{master_vip}")
       end
 
-      return true
+      true
     end
-    
+
     def set_internal_vip(vip, service, serf_key)
       etc_hosts_updated = false
       master_vip = vip || find_vip_from_serf(serf_key)
       etc_hosts_updated = update_hosts_file('/etc/hosts', master_vip, service, vip) if master_vip
-      return etc_hosts_updated
+      etc_hosts_updated
     end
   end
 end
