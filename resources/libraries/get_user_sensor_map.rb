12caadea0b9a8766061494a84a79af62eaed26af
Pablo Pérez <pperez@redborder.com>
2025-02-19 12:30:34 +0000

    Lint

--- resources/libraries/get_user_sensor_map.rb
+++ resources/libraries/get_user_sensor_map.rb
@@ -7,7 +7,7 @@ module RbManager
         m = Chef::Role.load m_key
         next unless m.override_attributes['redborder'] && m.override_attributes['redborder']['sso_idp_organization_name']
 
-        sensor_map_hash[m.override_attributes['redborder']['sso_idp_organization_name']] =  m.name.delete_prefix('rBsensor-').to_i
+        sensor_map_hash[m.override_attributes['redborder']['sso_idp_organization_name']] = m.name.delete_prefix('rBsensor-').to_i
       end
 
       sensor_map_str = sensor_map_hash.map { |name, sensor_id| "#{name}: #{sensor_id}" }.join("\n")
