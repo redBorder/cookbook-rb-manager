6281f9df29d04887dc345a86df3ab0f764145552
ljblancoredborder <ljblanco@redborder.com>
2025-06-30 18:06:11 +0100

    adding nginx as a vip and as balanced service for clustering

--- resources/attributes/default.rb
+++ resources/attributes/default.rb
@@ -219,8 +219,25 @@ default['redborder']['manager']['balanced'] = [ { port: 443, protocol: 'tcp', na
 default['redborder']['druid']['historical']['tier'] = 'default'
 default['redborder']['druid']['historical']['maxsize'] = -1
 
+# Balanced services
+default['redborder']['manager']['balanced'] = [
+  { service: 'webui',   name: 'redborder webui',            port: 443,  protocol: 'tcp', redirected_service: 'nginx',   persistence_timeout: 9600 },
+  { service: 'nginx',   name: 'nginx',                      port: 443,  protocol: 'tcp', redirected_service: 'nginx',   persistence_timeout: 9600 },
+  { service: 'f2k',     name: 'netflow,ipfix/sflow daemon', port: 2055, protocol: 'udp', redirected_service: 'f2k',     persistence_timeout: 30 },
+  { service: 'sfacctd', name: 'sflow daemon',               port: 6343, protocol: 'udp', redirected_service: 'sfacctd', persistence_timeout: 30 },
+  { service: 'kafka',   name: 'kafka',                      port: 9092, protocol: 'tcp', redirected_service: 'kafka',   persistence_timeout: 30 }
+]
+
 # Virtual Ips
-default['redborder']['manager']['virtual_ips'] = { external: [ { service: 'webui', deps: ['nginx'] }, { service: 'f2k' }, { service: 'sfacctd' }, { service: 'kafka' } ] }
+default['redborder']['manager']['virtual_ips'] = {
+  external: [
+    { service: 'webui', deps: ['nginx'] },
+    { service: 'nginx' },
+    { service: 'f2k' },
+    { service: 'sfacctd' },
+    { service: 'kafka' }
+  ]
+}
 
 default['redborder']['pending_changes'] = 0
 
