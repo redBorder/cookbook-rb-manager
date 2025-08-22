5260a476137e8975c4ca0768b9c934c6f61604b5
Juan Soto <jsoto@redborder.com>
2025-08-22 09:05:31 +0100

    Fix typo in rb_host_discovery

--- resources/libraries/get_indexer_tasks.rb
+++ resources/libraries/get_indexer_tasks.rb
@@ -10,7 +10,7 @@ module RbManager
         { task_name: 'rb_scanner', feed: 'rb_scanner_post' },
         { task_name: 'rb_location', feed: 'rb_loc_post' },
         { task_name: 'rb_wireless', feed: 'rb_wireless' },
-        { task_name: 'rb_host_discoveryt', feed: 'rb_host_discovery' },
+        { task_name: 'rb_host_discovery', feed: 'rb_host_discovery' },
       ]
 
       kafka_brokers = node['redborder']['managers_per_services']['kafka']
