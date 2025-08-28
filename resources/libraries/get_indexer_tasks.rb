20c9a5cdc8dbe66f0d53343f16fd10e830ac6d13
nilsver <nverschaeve@redborder.com>
2025-08-28 08:12:14 +0100

    different handling for monitor

--- resources/libraries/get_indexer_tasks.rb
+++ resources/libraries/get_indexer_tasks.rb
@@ -17,22 +17,34 @@ module RbManager
       kafka_brokers = kafka_brokers.map { |broker| "#{broker}.node:9092" }
       namespaces = node.run_state['namespaces']
 
-      base_tasks.flat_map do |task|
+      # Process all tasks EXCEPT rb_monitor
+      other_tasks = base_tasks.select { |t| t[:task_name] != 'rb_monitor' }.flat_map do |task|
         default_task = { spec: task[:task_name], task_name: task[:task_name], namespace: '', feed: task[:feed], kafka_brokers: kafka_brokers }
-
         namespace_tasks = namespaces.map do |namespace|
           taskHash = { spec: task[:task_name], task_name: task[:task_name] + '_' + namespace, namespace: namespace, kafka_brokers: kafka_brokers }
           taskHash[:feed] = task[:feed] + '_' + namespace
-          taskHash[:feed] = 'rb_monitor_post_' + namespace if task[:task_name] == 'rb_monitor'
           taskHash
         end
-
-        if task[:task_name] == 'rb_monitor' && !namespaces.empty?
-          namespace_tasks
-        else
-          [default_task] + namespace_tasks
-        end
+        [default_task] + namespace_tasks
       end
+
+      # Process rb_monitor task separately
+      monitor_task = { task_name: 'rb_monitor', feed: 'rb_monitor' }
+      monitor_tasks = []
+      if !namespaces.empty?
+        # Namespaces exist: use rb_monitor_post
+        # Default task
+        monitor_tasks << { spec: monitor_task[:task_name], task_name: monitor_task[:task_name], namespace: '', feed: 'rb_monitor_post', kafka_brokers: kafka_brokers }
+        # Namespace tasks
+        namespaces.each do |namespace|
+          monitor_tasks << { spec: monitor_task[:task_name], task_name: monitor_task[:task_name] + '_' + namespace, namespace: namespace, feed: 'rb_monitor_post_' + namespace, kafka_brokers: kafka_brokers }
+        end
+      else
+        # No namespaces: use rb_monitor
+        monitor_tasks << { spec: monitor_task[:task_name], task_name: monitor_task[:task_name], namespace: '', feed: 'rb_monitor', kafka_brokers: kafka_brokers }
+      end
+
+      other_tasks + monitor_tasks
     end
   end
 end
