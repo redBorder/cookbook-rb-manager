599779ad4377aca1b7df1ed6ff557274fa2c14f7
Rafael Gomez <rgomez@redborder.com>
2025-10-21 09:42:25 +0100

    Pass redis sentinel port to airflow_common

--- resources/recipes/configure.rb
+++ resources/recipes/configure.rb
@@ -556,6 +556,7 @@ if manager_services['airflow-scheduler'] || manager_services['airflow-webserver'
     airflow_webserver_hosts node['redborder']['managers_per_services']['airflow-webserver']
     redis_hosts node['redborder']['managers_per_services']['redis']
     redis_port node['redis']['port']
+    redis_sentinel_port node['redis']['sentinel_port']
     cpu_cores node['cpu']['total'].to_i
     ram_memory_kb node['memory']['total'].to_i
     action :add
