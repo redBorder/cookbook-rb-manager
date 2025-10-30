ea7a144d1b42a01dffaf14fd0e6f25b9cc0b59c1
Rafael Gomez <rgomez@redborder.com>
2025-10-30 14:21:39 +0000

    Add airflow-triggerer and airflow-dag-processor to rb-manager

--- resources/libraries/enables_celery_worker.rb
+++ resources/libraries/enables_celery_worker.rb
@@ -1,11 +1,17 @@
 module RbManager
   module Helpers
     def enables_celery_worker?
-      scheduler_hosts = node['redborder']['managers_per_services']['airflow-scheduler']
-      webserver_hosts = node['redborder']['managers_per_services']['airflow-webserver']
-      return false if scheduler_hosts.nil? || webserver_hosts.nil?
+      services = %w[
+        airflow-scheduler
+        airflow-webserver
+        airflow-dag-processor
+        airflow-triggerer
+      ]
 
-      all_hosts = (scheduler_hosts + webserver_hosts).uniq
+      all_hosts = services.flat_map { |svc|
+        node['redborder']['managers_per_services'][svc] || []
+      }.uniq
+
       all_hosts.size > 1
     end
   end
