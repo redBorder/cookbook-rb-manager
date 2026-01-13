6875f0f3e112d6bda1083b51c2dc7b8ebb664f5f
Miguel Negrón <45871721+manegron@users.noreply.github.com>
2026-01-13 15:10:38 +0000

    Add snmp, redfish and ipmi sensors (#413)

--- resources/libraries/get_sensors_all_info.rb
+++ resources/libraries/get_sensors_all_info.rb
@@ -2,7 +2,7 @@ module RbManager
   module Helpers
     def get_sensors_all_info
       sensors_info = {}
-      sensor_types = %w(ips-sensor ipsv2-sensor ipscp-sensor ipsg-sensor vault-sensor flow-sensor arubacentral-sensor mse-sensor meraki-sensor cisco-cloudproxy proxy-sensor scanner-sensor mse-sensor meraki-sensor ale-sensor cep-sensor device-sensor)
+      sensor_types = %w(ips-sensor ipsv2-sensor ipscp-sensor ipsg-sensor vault-sensor flow-sensor arubacentral-sensor mse-sensor meraki-sensor cisco-cloudproxy proxy-sensor scanner-sensor mse-sensor meraki-sensor ale-sensor cep-sensor device-sensor snmp-sensor redfish-sensor ipmi-sensor)
 
       sensor_types.each do |s_type|
         sensors = search(:node, "role:#{s_type}").sort  # get all s_type's sensor
