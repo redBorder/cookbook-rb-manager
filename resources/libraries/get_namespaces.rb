3586b2b84eb0a83d2c9debd740851b3e9ae0424a
manegron <manegron@redborder.com>
2026-01-29 16:09:43 +0000

    Add rescue to namespaces and org

--- resources/libraries/get_namespaces.rb
+++ resources/libraries/get_namespaces.rb
@@ -3,7 +3,13 @@ module RbManager
     def get_namespaces
       namespaces = []
       Chef::Role.list.each_key do |rol|
-        ro = Chef::Role.load rol
+        ro = nil
+        begin
+          ro = Chef::Role.load rol
+        rescue
+          Chef::Log.error("[get_namespaces] Failed to load role: #{role}")
+        end
+        
         next unless ro && ro.override_attributes['redborder'] && ro.override_attributes['redborder']['namespace'] && ro.override_attributes['redborder']['namespace_uuid'] && !ro.override_attributes['redborder']['namespace_uuid'].empty?
 
         namespaces.push(ro.override_attributes['redborder']['namespace_uuid'])
