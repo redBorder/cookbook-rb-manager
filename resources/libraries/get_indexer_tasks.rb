d54ed0fb46b43d135fd860f6bd773be4cb1fb134
José Jiménez <jjimenez@redborder.com>
2026-07-09 15:52:00 +0100

    add trap task

--- resources/libraries/get_indexer_tasks.rb
+++ resources/libraries/get_indexer_tasks.rb
@@ -16,6 +16,7 @@ module RbManager
         { task_name: 'rb_wireless', feed: 'rb_wireless' },
         { task_name: 'rb_malware', feed: 'rb_malware_post' },
         { task_name: 'rb_host_discovery', feed: 'rb_host_discovery' },
+        { task_name: 'rb_trap', feed: 'rb_trap_post'},
       ]
 
       # This was an optimization: if there are no namespaces, monitor pipeline is not active in Logstash.
