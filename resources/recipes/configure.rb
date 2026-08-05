e0a306e3d0883005fed6f4daccd4fb0a87baef19
David Vanhoucke <dvanhoucke@redborder.com>
2026-08-05 13:38:39 +0100

    fix lint

--- resources/recipes/configure.rb
+++ resources/recipes/configure.rb
@@ -566,7 +566,7 @@ redis_config 'Configure redis' do
   end
 end
 
-rb_alarm_engine_config "Configure redBorder alarm engine" do
+rb_alarm_engine_config 'Configure redBorder alarm engine' do
   if manager_services['redborder-alarm-engine']
     redis_password redis_secrets['pass'] unless redis_secrets.empty?
   else
